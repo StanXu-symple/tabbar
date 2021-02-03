acf5deb3f46f5f92a57d6784915f6e3d32faf21c
StanXu <69210103+StanXu-symple@users.noreply.github.com>
2021-02-03 21:11:11 +0800

    整合最终版

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,18 @@
 <template>
   <div id="app">
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/shouye.svg" alt="">
-        <img slot="item-active" src="./assets/img/tabbar/chuanshu.svg" alt="">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/fenlei.svg" alt="">
-        <img slot="item-active" src="./assets/img/tabbar/chuanshu.svg" alt="">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/gouwuche.svg" alt="">
-        <img slot="item-active" src="./assets/img/tabbar/chuanshu.svg" alt="">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/wode.svg" alt="">
-        <img slot="item-active" src="./assets/img/tabbar/chuanshu.svg" alt="">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar></main-tab-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TabBar from "@/components/tabbar/TabBar";
-import TabBarItem from "@/components/tabbar/TabBarItem";
+
+import MainTabBar from "@/components/maintabbar/MainTabBar";
 
 export default {
   name: 'App',
   components: {
-    TabBar,
-    TabBarItem
+    MainTabBar
   }
 }
 </script>
